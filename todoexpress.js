d48351896aab802c7d7a990a43edf8dedb89fd74
tracykaur <tracykaur@yahoo.com>
2017-11-21 16:34:39 +1000

    get, put, delete

--- todoexpress.js
+++ todoexpress.js
@@ -15,13 +15,30 @@ app.use(bodyParser.json())
 app.get('/api/todos', (req, res) => {
   res.send(todos);
 })
+app.get('/api/todos/:id', (req, res) => {
+  res.send(todos.filter(todo => {
+    return todo.id === parseInt(req.params.id)
+  }));
+});
 
 app.post('/api/todos', (req, res) => {
-  req.body.id = id++; 
+  req.body.id = id++;
   todos.push(req.body);
   res.send(todos);
 })
 
+app.put('/api/todos/:id', (req, res) => {
+  todosIndex = todos.findIndex(todo => todo.id === parseInt(req.params.id));
+  todos[todosIndex].item = req.body.item;
+  todos[todosIndex].done = req.body.done;
+  res.send(todos);
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+  todos = todos.findIndex(todo => todo.id !== parseInt(req.params.id));
+
+  res.send(todos);
+})
 
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!")
