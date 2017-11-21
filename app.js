0224d0410f5eba7a83d9bbe5acbc317540f3764f
tracykaur <tracykaur@yahoo.com>
2017-11-21 11:34:59 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3000;
+
+let students = ['Simon', 'Scott'];
+
+function handleMyRequest(request, response) {
+  if (request.url === '/students') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    response.end(JSON.stringify(students));
+  } else if (request.url === '/bye') {
+    response.end('Goodbye')
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+}
+
+const server = http.createServer(handleMyRequest)
+
+console.log(`Server is running on port ${port}`)
+server.listen(port);
---
+++ todo.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const port = 3000;
+
+let todos = {
+  item :'wash clothes', done: false
+};
+
+function handleMyRequest(request, response) {
+  if (request.url === '/todos') {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    response.end(JSON.stringify(todos));
+  } else if (request.url === '/bye') {
+    response.end('Goodbye')
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+}
+
+const server = http.createServer(handleMyRequest)
+
+console.log(`Server is running on port ${port}`)
+server.listen(port);
